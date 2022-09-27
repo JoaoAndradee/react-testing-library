d77dea91828ff9a0a82edcdbe1e018dd78cb2f99
joao <joao.dev94@gmail.com>
2022-09-27 01:36:59 -0300

    Teste do component NotFound - Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste do component NotFound', () => {
+  test('A página contém um h2 com o texto page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  test('A página mostra a imagem com link certo', () => {
+    const { container } = renderWithRouter(<NotFound />);
+    const correctLinkImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgNotFound = container.getElementsByTagName('img')[0];
+    expect(imgNotFound.src).toBe(correctLinkImg);
+  });
+});
