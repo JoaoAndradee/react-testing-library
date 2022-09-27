f605ae014ca716c9ca3fa3c514242efdac91ca87
joao <joao.dev94@gmail.com>
2022-09-27 01:25:57 -0300

    Teste do component favoritePokemons - Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Teste do component FavoritePokemons', () => {
+  test('caso não tenha pokemons exibe a mensagem no favorite pokemons', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const empatyMessage = screen.getByText(/no favorite pokemon found/i);
+    expect(empatyMessage).toBeInTheDocument();
+  });
+
+  test('É exibido pokemons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const btnFire = screen.getByRole('button', { name: /fire/i });
+    userEvent.click(btnFire);
+    const btnDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(btnDetails);
+    const favCheck = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(favCheck);
+    act(() => {
+      history.push('/favorites');
+    });
+    const favPokemon = screen.getByText(/charmander/i);
+    expect(favPokemon).toBeInTheDocument();
+  });
+});
